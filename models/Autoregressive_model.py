4e9fc8a96d495970f6fc0841b2d7a3d85b58ae23
Khoawawa <atom08072004@gmail.com>
2025-11-02 16:15:04 +0700

    a

--- models/Autoregressive_model.py
+++ models/Autoregressive_model.py
@@ -125,7 +125,7 @@ class AutoregressiveModel(nn.Module):
                 return self.beam_search(generated,beam_size=self.beam_size,batch_size=B, max_length=max_length,device=x_visual.device)
             else:
                 return self.model.generate(
-                    inputs_embedas=prefix_embs,
+                    inputs_embeds=prefix_embs,
                     max_length=max_length + self.prefix_len,
                     num_beams=self.beam_size,
                     temperature=self.temperature,
